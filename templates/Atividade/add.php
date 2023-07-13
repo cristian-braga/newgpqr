17c2d714cfe9a80a7bb3f8e1b218ed8605ad0cdc
Cristian Braga <crisllopes10@hotmail.com>
2023-07-13 16:00:19 -0300

    :sparkles: alterações na controller atividade

--- templates/Atividade/add.php
+++ templates/Atividade/add.php
@@ -1,11 +1,3 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Atividade $atividade
- * @var \Cake\Collection\CollectionInterface|string[] $servico
- * @var \Cake\Collection\CollectionInterface|string[] $statusAtividade
- */
-?>
 <div class="row">
     <aside class="column">
         <div class="side-nav">
@@ -15,21 +7,58 @@
     </aside>
     <div class="column-responsive column-80">
         <div class="atividade form content">
-            <?= $this->Form->create($atividade) ?>
+            <?= $this->Form->create($atividade, ['id' => 'form']) ?>
             <fieldset>
                 <legend><?= __('Add Atividade') ?></legend>
                 <?php
-                    echo $this->Form->control('job');
-                    echo $this->Form->control('data_postagem', ['empty' => true]);
-                    echo $this->Form->control('remessa_atividade');
-                    echo $this->Form->control('quantidade_documentos');
+                    echo $this->Form->control('job', ['name' => 'job[]']);
+                    echo $this->Form->control('data_postagem', ['name' => 'data_postagem[]', 'value' => date('Y-m-d')]);
+                    echo $this->Form->control('remessa_atividade', ['name' => 'remessa_atividade[]']);
+                    echo $this->Form->control('quantidade_documentos', ['name' => 'quantidade_documentos[]']);
                     echo $this->Form->label('recibo_postagem');
-                    echo $this->Form->select('recibo_postagem', [0, 1, 2, 3], ['empty' => '-- Selecione --']);
-                    echo $this->Form->control('servico_id', ['options' => $servico, 'empty' => '-- Selecione --']);
+                    echo $this->Form->select('recibo_postagem', [0, 1, 2, 3], ['empty' => '-- Selecione --', 'name' => 'recibo_postagem[]']);
+                    echo $this->Form->control('servico', ['options' => $servico, 'empty' => '-- Selecione --', 'name' => 'servico_id[]']);
                 ?>
             </fieldset>
+            <div id="btn_add">
+                <?= $this->Form->button('+', ['type' => 'button', 'id' => 'add_campo']) ?>
+            </div>
             <?= $this->Form->button(__('Submit')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
 </div>
+
+<script>
+    const form = document.getElementById('form');
+    const div_btn = document.getElementById('btn_add');
+    const btn_add = document.getElementById('add_campo');
+    const campos_max = 20;
+    let x = 0;
+
+    btn_add.addEventListener('click', function() {
+        if (x < campos_max) {
+            const novo_campo = document.createElement('fieldset');
+            novo_campo.innerHTML = '\
+                <legend><?= __('Add Atividade') ?></legend>\
+                <?= $this->Form->control('job', ['name' => 'job[]', 'maxlenght' => 10]) ?>\
+                <?= $this->Form->control('data_postagem', ['name' => 'data_postagem[]', 'type' => 'date', 'value' => date('Y-m-d')]) ?>\
+                <?= $this->Form->control('remessa_atividade', ['name' => 'remessa_atividade[]', 'maxlenght' => 11]) ?>\
+                <?= $this->Form->control('quantidade_documentos', ['name' => 'quantidade_documentos[]', 'type' => 'number']) ?>\
+                <?= $this->Form->label('recibo_postagem') ?>\
+                <?= $this->Form->select('recibo_postagem', [0, 1, 2, 3], ['empty' => '-- Selecione --', 'name' => 'recibo_postagem[]']) ?>\
+                <?= $this->Form->control('servico', ['options' => $servico, 'empty' => '-- Selecione --', 'name' => 'servico_id[]']) ?>\
+                <?= $this->Form->button('-', ['type' => 'button', 'onclick' => 'del_campo(this)']) ?>\
+            ';
+
+        form.insertBefore(novo_campo, div_btn);
+        btn_add.blur();
+        x++;
+        }
+    });
+
+    function del_campo(btn_del) {
+        btn_del.parentNode.remove();
+        x--;
+    }
+</script>
