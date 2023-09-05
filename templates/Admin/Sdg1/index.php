414803026b62eef7805e274d0c97431283f65cf9
italloalves99 <134318969+italloalves99@users.noreply.github.com>
2023-09-05 15:27:46 -0300

    remessas comment

--- templates/Admin/Sdg1/index.php
+++ templates/Admin/Sdg1/index.php
@@ -22,7 +22,7 @@
                     <td>               
                         <?= $this->Html->link(__('Editar'), ['action' => 'edit', $sdg1->id], ['class' => 'btn btn-outline-warning btn-sm btn-shadow']) ?>
                         <?= $this->Form->postLink(__('Excluir'), ['action' => 'delete', $sdg1->id], ['class' => 'btn btn-outline-danger btn-sm btn-shadow'], ['confirm' => __('Are you sure you want to delete # {0}?', $sdg1->id)]) ?>
-                        <?= $this->Html->link(__('PDF'), ['action' => 'view', $sdg1->id], ['class' => 'btn btn-outline-primary btn-sm btn-shadow']) ?>
+                        <?= $this->Html->link(__('PDF'), ['action' => 'pdf', $sdg1->id], ['class' => 'btn btn-outline-primary btn-sm btn-shadow', 'target' => '_blank']) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
