0c4d05328b66a0c5699b2b64b01ad7291b127f28
giovanni-jo-oliveira <110416240+giovanni-jo-oliveira@users.noreply.github.com>
2023-09-20 17:52:05 -0300

    correção add remessas pmmg

--- templates/Admin/Saalm/edit.php
+++ templates/Admin/Saalm/edit.php
@@ -1,51 +1,38 @@
 <h3 class="text-center mt-2 mb-4">EDITAR</h3>
-<?= $this->Form->create($saalm,['class' => 'mx-auto p-3 form', 'style' => 'width: 60%']) ?>
-<div class="row g-3">
+<?= $this->Form->create($saalm,['class' => 'mx-auto p-3 form', 'style' => 'width: 50%']) ?>
+<div class="row g-3 mt-3">
     <div class="col-md-6">
-        <label for="sistema">Serviço:</label>
-        <div class="form-check form-check-inline">
-            <label><b>SAALM005</b></label>
-        </div>
+        <label class="form-label">JOB</label>
+        <?php echo $this->Form->control('job', ['type' => 'number', 'class' => 'form-control', 'maxlenght' => 4, 'placeholder' => 'N° do Job', 'label' => false]); ?>
     </div>
-    <div class="row g-3">
-        <div class="col-md-6">
-            <label class="form-label">N° do JOB</label>
-            <?php echo $this->Form->control('job', ['type' => 'number', 'class' => 'form-control', 'maxlenght' => 4, 'placeholder' => 'Job', 'label' => false]); ?>
-        </div>
-
-        <div class="col-md-6">
-           <label class="form-label">Data</label>
-            <?php echo $this->Form->control('dataAtual', ['type' => 'date', 'class' => 'form-control', 'value' => date('Y-m-d'), 'required', 'label' => false]); ?>
-         </div>
+    <div class="col-md-6">
+        <label class="form-label">Data</label>
+        <?php echo $this->Form->control('dataAtual', ['type' => 'date', 'class' => 'form-control', 'value' => date('Y-m-d'), 'required', 'label' => false]); ?>
     </div>
-    <h6><b>Relatório Contábil:</b></h6>
-    <div class="row g-2">
-        <div class="col-md-6">
-            <label for="form-label">Cópias</label>
-            <input class="form-control" type="number" name="copias" id="copias" value="<?php echo $saalm['copias'] ?>">
-        </div>
-            <div class="col-md-6">
-                <label for="form-label">Páginas</label>
-                <input class="form-control" type="number" name="paginas" id="paginas"
-                value="<?php echo $saalm['paginas'] ?>">
-            </div>
+    <h5 class="text-center mt-3"><b>Relatório Contábil:</b></h5>
+    <div class="col-md-6">
+        <label class="form-label">Cópias</label>
+        <input class="form-control" type="number" name="copias" id="copias" value="<?php echo $saalm['copias'] ?>">
     </div>
-    <h6><b>Estatística:</b></h6>
-    <div class="row g-2">
-        <div class="col-md-6">
-                <label for="form-label">Cópias</label>
-                <input class="form-control" type="number" name="copias1" id="copias1"
-                value="<?php echo $saalm['copias1'] ?>">
-        </div>
-        <div class="col-md-6">
-            <label for="form-label">Páginas</label>
-            <input class="form-control" type="number" name="paginas1" id="paginas1"
-            value="<?php echo $saalm['paginas1'] ?>">
-        </div>
+    <div class="col-md-6">
+        <label class="form-label">Páginas</label>
+        <input class="form-control" type="number" name="paginas" id="paginas"
+        value="<?php echo $saalm['paginas'] ?>">
     </div>
-        <div class="col-md-6" style="margin-top:15px;">
-            <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
-            <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary'], ['style' => 'margin-left:15px;']) ?>
-            <?= $this->Form->end() ?>
-        </div>
+    <h5 class="text-center mt-3"><b>Estatística:</b></h5>
+    <div class="col-md-6">
+        <label class="form-label">Cópias</label>
+        <input class="form-control" type="number" name="copias1" id="copias1"
+        value="<?php echo $saalm['copias1'] ?>">
+    </div>
+    <div class="col-md-6">
+        <label class="form-label">Páginas</label>
+        <input class="form-control" type="number" name="paginas1" id="paginas1"
+        value="<?php echo $saalm['paginas1'] ?>">
+    </div>
+    <div class="col-md-6" style="margin-top:15px;">
+        <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
+        <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary'], ['style' => 'margin-left:15px;']) ?>
+        <?= $this->Form->end() ?>
+    </div>
 </div>
--- templates/Admin/Smafe008b/add.php
+++ templates/Admin/Smafe008b/add.php
@@ -1,31 +1,31 @@
-<h3 class="text-center">Cadastrar</h3><hr>
-<?= $this->Form->create($smafe008b, ['id' => 'form', 'class' => 'mx-auto p-3 form']) ?>
-<div class="row">
-    <div class="col-md-2">
+<h3 class="text-center mt-2 mb-4">CADASTRAR</h3>
+<?= $this->Form->create($smafe008b, ['id' => 'form', 'class' => 'mx-auto p-3 form',  'style' => 'width: 50%']) ?>
+<div class="row g-3 mt-3">
+    <div class="col-md-6">
         <label class="form-label">Cópias</label>
         <?= $this->Form->control('copias', ['type' => 'number', 'class' => 'form-control', 'maxlenght' => 10, 'placeholder' => 'Cópias', 'label' => false]) ?>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-6">
         <label class="form-label">Páginas</label>
         <?= $this->Form->control('paginas', ['type' => 'number', 'class' => 'form-control', 'maxlenght' => 10, 'placeholder' => 'Páginas', 'label' => false]) ?>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-6">
         <label class="form-label">Concurso</label>
         <?= $this->Form->control('concurso', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Concurso', 'label' => false]) ?>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-6">
         <label class="form-label">Job</label>
         <?= $this->Form->control('job', ['type' => 'number', 'class' => 'form-control', 'maxlenght' => 4, 'placeholder' => 'Job', 'label' => false]) ?>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-6">
         <label class="form-label">Referência</label>
         <?= $this->Form->control('referencia', ['type' => 'month', 'class' => 'form-control', 'placeholder' => 'Referência', 'label' => false]) ?>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-6">
         <label class="form-label">Data</label>
         <?= $this->Form->control('data', ['type' => 'date', 'class' => 'form-control', 'value' => date('Y-m-d'), 'required', 'label' => false]) ?>
     </div>
-    <div class="mt-3">
+    <div class="col-12 mt 5">
         <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
         <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary']) ?>
     </div>
