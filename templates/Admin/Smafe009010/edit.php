75b5bd5c6d33d52933554fa412e28cfd24370399
giovanni-jo-oliveira <110416240+giovanni-jo-oliveira@users.noreply.github.com>
2023-09-20 19:19:24 -0300

    telas add remessas pmmg

--- templates/Admin/Smafe009010/edit.php
+++ templates/Admin/Smafe009010/edit.php
@@ -1,33 +1,33 @@
-<h3 class="text-center">Editar</h3>
-<?= $this->Form->create($smafe009010, ['class' => 'mx-auto p-3 form', 'style' => 'width: 60%']) ?>
-<div class="row">
+<h3 class="text-center mt-2 mb-4">EDITAR</h3>
+<?= $this->Form->create($smafe009010, ['class' => 'mx-auto p-3 form', 'style' => 'width: 50%']) ?>
+<div class="row g-3 mt-3">
     <div class="col-md-6">
         <label class="form-label">Serviço:</label>
         <?= $this->Form->select('servico', ['SMAFE009' => 'SMAFE009', 'SMAFE010' => 'SMAFE010'], ['class' => 'form-select', 'required', 'label' => false]) ?>
     </div>
     <div class="col-md-6">
         <label class="form-label">Referência</label>
-        <?php echo $this->Form->control('referencia', ['type' => 'month', 'class' => 'form-control', 'autocomplete' => 'off', 'label' => false]); ?>
+        <?= $this->Form->control('referencia', ['type' => 'month', 'class' => 'form-control', 'required', 'label' => false]) ?>
     </div>
     <div class="col-md-6">
-        <label class="form-label">Data</label>
-        <?php echo $this->Form->control('data', ['type' => 'date', 'class' => 'form-control', 'autocomplete' => 'off', 'label' => false]); ?>
+        <label class="form-label">Data Atual</label>
+        <?= $this->Form->control('data', ['type' => 'date', 'class' => 'form-control', 'required', 'label' => false]) ?>
     </div>
     <div class="col-md-6">
         <label class="form-label">Concurso</label>
-        <?php echo $this->Form->control('concurso', ['type' => 'number', 'class' => 'form-control', 'autocomplete' => 'off', 'label' => false]); ?>
+        <?= $this->Form->control('concurso', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Concurso', 'required', 'label' => false]) ?>
     </div>
     <div class="col-md-6">
         <label class="form-label">Quantidade de Etiquetas</label>
-        <?php echo $this->Form->control('quantidade_etiquetas', ['type' => 'number', 'class' => 'form-control', 'autocomplete' => 'off', 'label' => false]); ?>
+        <?= $this->Form->control('quantidade_etiquetas', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Etiquetas', 'required', 'label' => false]) ?>
     </div>
     <div class="col-md-6">
-        <label class="form-label">Job</label>
-        <?php echo $this->Form->control('job', ['type' => 'number', 'class' => 'form-control', 'autocomplete' => 'off', 'label' => false]); ?>
+        <label class="form-label">JOB</label>
+        <?= $this->Form->control('job', ['type' => 'number', 'class' => 'form-control', 'required', 'placeholder' => 'JOB', 'label' => false]) ?>
     </div>
-</div>
-<div class="col-12 mt-2">
-    <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
-    <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary'], ['style' => 'margin-left:15px;', 'label' => false]) ?>
-    <?= $this->Form->end() ?>
+    <div class="col-md-6" style="margin-top:15px;">
+        <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
+        <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary'], ['style' => 'margin-left:15px;', 'label' => false]) ?>
+        <?= $this->Form->end() ?>
+    </div>
 </div>
