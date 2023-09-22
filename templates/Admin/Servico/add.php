79aa0a5ff22308fafc144b82348c7d1c1cfd9129
Cristian Braga <crisllopes10@hotmail.com>
2023-09-22 14:15:23 -0300

    Cadastro de Serviço

--- templates/Admin/Servico/add.php
+++ templates/Admin/Servico/add.php
@@ -1,41 +1,73 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Servico $servico
- */
-?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Servico'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+<h3 class="text-center mt-2 mb-4">CADASTRAR SERVIÇO</h3>
+<?= $this->Form->create($servico, ['class' => 'mx-auto p-3 form', 'style' => 'width: 60%']) ?>
+    <div class="row g-3">
+        <div class="col-md-6">
+            <label class="form-label">Nome do Serviço</label>
+            <?= $this->Form->control('nome_servico', ['class' => 'form-control text-uppercase', 'required', 'label' => false]) ?>
         </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="servico form content">
-            <?= $this->Form->create($servico) ?>
-            <fieldset>
-                <legend><?= __('Add Servico') ?></legend>
-                <?php
-                    echo $this->Form->control('nome_servico');
-                    echo $this->Form->control('descricao_servico');
-                    echo $this->Form->control('cliente_responsavel_servico');
-                    echo $this->Form->control('cliente_servico');
-                    echo $this->Form->control('correios_servico');
-                    echo $this->Form->control('impressao_servico');
-                    echo $this->Form->control('tipo_impressao_servico');
-                    echo $this->Form->control('tipo_preparo_servico');
-                    echo $this->Form->control('envelopamento_servico');
-                    echo $this->Form->control('separador_servico');
-                    echo $this->Form->control('entrega_servico');
-                    echo $this->Form->control('cod_sistema_servico');
-                    echo $this->Form->control('descricao_sistema_servico');
-                    echo $this->Form->control('valor_servico');
-                    echo $this->Form->control('folha_rosto');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+        <div class="col-md-6">
+            <label class="form-label">Código do Sistema</label>
+            <?= $this->Form->control('cod_sistema_servico', ['class' => 'form-control text-uppercase', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-12">
+            <label class="form-label">Cliente Responsável Serviço</label>
+            <?= $this->Form->control('cliente_responsavel_servico', ['class' => 'form-control', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-12">
+            <label class="form-label">Cliente Serviço</label>
+            <?= $this->Form->control('cliente_servico', ['class' => 'form-control', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-8">
+            <label class="form-label">Descrição Serviço</label>
+            <?= $this->Form->control('descricao_servico', ['options' => $descricao, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-4">
+            <label class="form-label">Ativo</label>
+            <?= $this->Form->control('ativo', ['options' => $ativo, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Correios Serviço</label>
+            <?= $this->Form->control('correios_servico', ['options' => $correios, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Impressão Serviço</label>
+            <?= $this->Form->control('impressao_servico', ['options' => $impressao_servico, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Tipo de Impressão</label>
+            <?= $this->Form->control('tipo_impressao_servico', ['options' => $tipo_impressao, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Tipo de Preparo</label>
+            <?= $this->Form->control('tipo_preparo_servico', ['options' => $tipo_preparo, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Tipo de Envelopamento</label>
+            <?= $this->Form->control('envelopamento_servico', ['options' => $env_servico, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Separador</label>
+            <?= $this->Form->control('separador_servico', ['options' => $separador, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Tipo de Entrega</label>
+            <?= $this->Form->control('entrega_servico', ['options' => $entrega, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Descrição do Sistema</label>
+            <?= $this->Form->control('descricao_sistema_servico', ['options' => $desc_sistema, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Valor</label>
+            <?= $this->Form->control('valor_servico', ['options' => $valor_servico, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-6">
+            <label class="form-label">Folhas de Rosto</label>
+            <?= $this->Form->control('folha_rosto', ['options' => $folha_rosto, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-12 mt-5">
+            <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
+            <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary']) ?>
         </div>
     </div>
-</div>
+<?= $this->Form->end() ?>
--- templates/Admin/Servico/index.php
+++ templates/Admin/Servico/index.php
@@ -1,25 +1,23 @@
-<h2 class="text-center text-gpqr mt-2 mb-4">Serviço</h2>
+<h2 class="text-center text-gpqr mt-2 mb-4">SERVIÇO</h2>
 <?= $this->Html->link(__('Cadastrar'), ['action' => 'add'], ['class' => 'btn btn-secondary float-start mb-4']) ?>
 <div class="table-responsive table-gpqr">
-    <table class="table table-borderless table-hover table-striped text-center">
+    <table class="table table-borderless table-striped text-center align-middle">
         <thead>
             <tr>
                 <th>Serviço</th>
                 <th>Descrição</th>
                 <th>Responsável</th>
                 <th>Cliente</th>
-                <th>Sistema</th>
                 <th class="col-2">Ações</th>
             </tr>
         </thead>
-        <tbody class="align-middle">
+        <tbody>
             <?php foreach ($servico as $servico) : ?>
                 <tr>
                     <td><?= $this->Html->link($servico->nome_servico, ['action' => 'view', $servico->id], ['class' => 'custom-btn btn-gpqr-view']) ?></td>
                     <td><?= h($servico->descricao_servico) ?></td>
                     <td><?= h($servico->cliente_responsavel_servico) ?></td>
                     <td><?= h($servico->cliente_servico) ?></td>
-                    <td><?= h($servico->descricao_sistema_servico) ?></td>
                     <td>
                         <?= $this->Html->link(__('Editar'), ['action' => 'edit', $servico->id], ['class' => 'btn btn-outline-warning btn-sm btn-shadow']) ?>
                         <?= $this->Html->link(__('Excluir'), ['action' => 'delete', $servico->id], ['class' => 'btn btn-outline-danger btn-sm btn-shadow', 'confirm' => __('Realmente deseja excluir o serviço: {0}?', $servico->id)]) ?>
