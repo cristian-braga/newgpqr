2d3c9522d31131cc9c22b47c5edd647c5bf07d8b
giovanni-jo-oliveira <110416240+giovanni-jo-oliveira@users.noreply.github.com>
2023-09-22 15:25:09 -0300

    serviços internos e cd

--- templates/Admin/Cd/edit.php
+++ templates/Admin/Cd/edit.php
@@ -1,53 +1,37 @@
 <h3 class="text-center mt-2 mb-4">EDITAR</h3>
-<?= $this->Form->create($cd,['class' => 'mx-auto p-3 form', 'style' => 'width: 60%']) ?>
-<div class="row g-3">
+<?= $this->Form->create($cd,['class' => 'mx-auto p-3 form', 'style' => 'width: 50%']) ?>
+<div class="row g-3 mt-3">
     <div class="col-md-6">
-        <label for="sistema">Serviço:</label>
-        <div class="form-check form-check-inline">
-            <label><b>CD</b></label>
-        </div>
+        <label class="form-label">Data Atual</label>
+        <?= $this->Form->control('dataAtual', ['type' => 'date', 'class' => 'form-control', 'value' => date('Y-m-d'), 'required', 'label' => false]) ?>
     </div>
-    <div class="row g-3">
-        <div class="col-md-3">
-            <label class="form-label">Data Atual</label>
-            <?= $this->Form->control('dataAtual', ['type' => 'date', 'class' => 'form-control', 'value' => date('Y-m-d'), 'required', 'label' => false]) ?>
-        </div>
-        <div class="col-md-2">
-            <label class="form-label">Ocorrência</label>
-            <?php echo $this->Form->control('ocorrencia', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Ocorrência', 'label' => false]); ?>
-        </div>
-            <div class="col-md-2">
-            <label class="form-label">Quantidade</label>
-            <?php echo $this->Form->control('quantidade', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Quantidade', 'label' => false]); ?>
-        </div>
-        <div class="col-md-5">
-            <label class="form-label">Cliente</label>
-            <?php echo $this->Form->control('cliente', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Cliente', 'label' => false]); ?>
-        </div>
+    <div class="col-md-6">
+        <label class="form-label">Ocorrência</label>
+        <?php echo $this->Form->control('ocorrencia', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Ocorrência', 'label' => false]); ?>
     </div>
-    <div class="row g-3">
-        <div class="col-md-4">
-            <label class="form-label">Solicitante</label>
-            <?php echo $this->Form->control('solicitante', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Solicitante', 'label' => false]) ?>
-        </div>
-
-        <div class="col-md-4">
-            <label class="form-label">Descrição</label>
-            <?php echo $this->Form->control('descricao', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Descrição', 'label' => false]); ?>
-        </div>
-        <div class="col-md-4">
-            <label class="form-label">Observação Email:</label>
-            <?php echo $this->Form->control('observacao', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Observacao', 'label' => false]); ?>
-        </div>
-        <!-- <div class="col-md-2">
-            <label class="form-label">Observação Email:</label>
-            <?php echo $this->Form->control('funcionario', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Observacao', 'label' => false]); ?>
-        </div> -->
+    <div class="col-md-6">
+        <label class="form-label">Quantidade</label>
+        <?php echo $this->Form->control('quantidade', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Quantidade', 'label' => false]); ?>
     </div>
-    <div class="col-md-6" style="margin-top: 15px;">
+    <div class="col-md-6">
+        <label class="form-label">Cliente</label>
+        <?php echo $this->Form->control('cliente', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Cliente', 'label' => false]); ?>
+    </div>
+    <div class="col-md-6">
+        <label class="form-label">Solicitante</label>
+        <?php echo $this->Form->control('solicitante', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Solicitante', 'label' => false]) ?>
+    </div>
+    <div class="col-md-6">
+        <label class="form-label">Descrição</label>
+        <?php echo $this->Form->control('descricao', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Descrição', 'label' => false]); ?>
+    </div>
+    <div class="col-md-6">
+        <label class="form-label">Observação Email:</label>
+        <?php echo $this->Form->control('observacao', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Observacao', 'label' => false]); ?>
+    </div>
+    <div class="col-md-12" style="margin-top: 15px;">
         <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
         <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary'], ['style' => 'margin-left:15px;']) ?>
         <?= $this->Form->end() ?>
     </div>
-
 </div>
--- templates/Admin/Encadernacao/edit.php
+++ templates/Admin/Encadernacao/edit.php
@@ -1,31 +1,19 @@
-<h3 class="text-center mt-2 mb-4">Editar</h3>
-<?= $this->Form->create($encadernacao, ['class' => 'mx-auto p-3 form', 'style' => 'width: 60%']) ?>
-<div class="row g-3">
-    <div class="col-md-6">
-        <label class="form-label">Data</label>
-        <?php echo $this->Form->control('data_cadastro', ['type' => 'date', 'class' => 'form-control', 'value' => date('Y-m-d'), 'required', 'label' => false]); ?>
-    </div>
-    <div class="col-md-6">
+<h3 class="text-center mt-2 mb-4">EDITAR</h3>
+<?= $this->Form->create($encadernacao, ['class' => 'mx-auto p-3 form', 'style' => 'width: 50%']) ?>
+<div class="row g-3 mt-3">
+<div class="col-md-6">
         <label class="form-label">Ocorrência</label>
-        <?php echo $this->Form->control('ocorrencia', ['type' => 'number', 'class' => 'form-control', 'required', 'label' => false]); ?>
-    </div>
-    <div class="col-md-6">
-        <label class="form-label">Solicitante</label>
-        <?php echo $this->Form->control('solicitante', ['type' => 'text', 'class' => 'form-control', 'required', 'label' => false]); ?>
+        <?php echo $this->Form->control('ocorrencia', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Ocorrência', 'label' => false]); ?>
     </div>
     <div class="col-md-6">
         <label class="form-label">Descrição</label>
-        <?php echo $this->Form->control('descricao', ['type' => 'text', 'class' => 'form-control', 'required', 'label' => false]); ?>
+        <?php echo $this->Form->control('descricao', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Descrição', 'label' => false]); ?>
     </div>
     <div class="col-md-6">
-        <label class="form-label">Páginas</label>
-        <?php echo $this->Form->control('paginas', ['type' => 'number', 'class' => 'form-control', 'required', 'label' => false]); ?>
+        <label class="form-label">Solicitante</label>
+        <?php echo $this->Form->control('solicitante', ['type' => 'text', 'class' => 'form-control', 'placeholder' => 'Solicitante', 'label' => false]); ?>
     </div>
     <div class="col-md-6">
-        <label class="form-label">Cópias</label>
-        <?php echo $this->Form->control('copias', ['type' => 'number', 'class' => 'form-control', 'required', 'label' => false]); ?>
-    </div>
-    <div class="col-md-6 mt-2">
         <label class="form-label">Tipo de Capa</label>
         <select class="form-select" name ="tipo_capa">
             <option selected>Selecione:</option>
@@ -33,21 +21,21 @@
             <option value="Capa PRODEMGE">Capa - Prodemge</option>
         </select>
     </div>
-    <!-- <?php
-                    echo $this->Form->control('descricao');
-                    echo $this->Form->control('paginas');
-                    echo $this->Form->control('ocorrencia');
-                    echo $this->Form->control('solicitante');
-                    echo $this->Form->control('data_cadastro');
-                    echo $this->Form->control('funcionario');
-                    echo $this->Form->control('tipo_capa');
-                    echo $this->Form->control('copias');
-                    echo $this->Form->control('total');
-                    echo $this->Form->control('capas');
-                ?> -->
-    <div class="col-12 mt-3">
+    <div class="col-md-6">
+        <label class="form-label">Páginas</label>
+        <?php echo $this->Form->control('paginas', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Quant. de Páginas', 'label' => false]); ?>
+    </div>
+    <div class="col-md-6">
+        <label class="form-label">Cópias</label>
+        <?php echo $this->Form->control('copias', ['type' => 'number', 'class' => 'form-control', 'placeholder' => 'Cópias', 'label' => false]); ?>
+    </div>
+    <div class="col-md-6">
+        <label class="form-label">Data</label>
+        <?php echo $this->Form->control('data_cadastro', ['type' => 'date', 'class' => 'form-control', 'value' => date('Y-m-d'), 'required', 'label' => false]); ?>
+    </div>
+    <div class="col-md-12" style="margin-top:15px;">
         <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
-        <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary']) ?>
+        <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary'], ['style' => 'margin-left:15px;']) ?>
+        <?= $this->Form->end() ?>
     </div>
 </div>
-<?= $this->Form->end() ?>
