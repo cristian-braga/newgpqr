2282fae08225237e325648e868f757f55fd9b94e
Cristian Braga <crisllopes10@hotmail.com>
2023-10-02 11:06:16 -0300

    fix: Alteração da página Digitalizar

--- templates/Admin/Digitalizacao/edit.php
+++ templates/Admin/Digitalizacao/edit.php
@@ -1,23 +1,21 @@
-<h3 class="text-center mt-2 mb-4">EDITAR DIGITALIZACAO</h3>
-
-<?= $this->Form->create($digitalizacao, ['id' => 'form', 'class' => 'mx-auto p-3 form ']) ?>
-
-<div class="row">
-    <div class="col-md-3">
-        <label class="form-label">Serviço</label>
-        <?= $this->Form->control('servico_id[]', ['options' => $servico, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+<h3 class="text-center mt-2 mb-4">EDITAR</h3>
+<?= $this->Form->create($digitalizacao, ['class' => 'mx-auto p-3 form', 'style' => 'width: 35%']) ?>
+    <div class="row g-3">
+        <div class="col-md-12">
+            <label class="form-label">Serviço</label>
+            <?= $this->Form->control('servico_id', ['options' => $servicos, 'class' => 'form-select', 'empty' => '-- Selecione --', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-12">
+            <label class="form-label">Documentos</label>
+            <?= $this->Form->control('quantidade_documentos', ['type' => 'number', 'class' => 'form-control', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-md-12">
+            <label class="form-label">Período</label>
+            <?= $this->Form->control('periodo', ['type' => 'month', 'class' => 'form-control', 'required', 'label' => false]) ?>
+        </div>
+        <div class="col-12 mt-5">
+            <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
+            <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary']) ?>
+        </div>
     </div>
-    <div class="col-md-3">
-        <label class="form-label">Quantidade de Documentos</label>
-        <?= $this->Form->control('quantidade_documentos[]', ['type' => 'number', 'class' => 'form-control', 'maxlenght' => 10, 'placeholder' => 'Quantidade', 'label' => false]) ?>
-    </div>
-    <div class="col-md-3">
-        <label class="form-label">Período</label>
-        <?= $this->Form->control('periodo[]', ['type' => 'month', 'class' => 'form-control', 'maxlenght' => 11, 'label' => false]) ?>
-    </div>
-</div>
-<div class="mt-5">
-    <?= $this->Form->button(__('Salvar'), ['class' => 'btn btn-primary']) ?>
-    <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-secondary']) ?>
-</div>
 <?= $this->Form->end() ?>
--- templates/Admin/Digitalizacao/index.php
+++ templates/Admin/Digitalizacao/index.php
@@ -1,57 +1,54 @@
-<h2 class="text-center text-gpqr mt-2 mb-4">Digitalização</h2>
-
-<div class="conteudo mt-5">
+<h2 class="text-center text-gpqr mt-2 mb-4">DIGITALIZAÇÃO</h2>
+<?= $this->Html->link(__('Cadastrar'), ['action' => 'add'], ['class' => 'btn btn-secondary']) ?>
+<div class="conteudo">
     <?= $this->Form->create(null, ['type' => 'get']) ?>
-    <div class="row g-3">
-        <div class="col-md-2">
-            <label class="form-label">Serviço:</label>
-            <?= $this->Form->control('servico', ['options' => $servicos, 'class' => 'form-select', 'empty' => '-- Selecione --', 'label' => false]) ?>
+        <div class="row g-3">
+            <div class="col-md-2">
+                <label class="form-label">Serviço:</label>
+                <?= $this->Form->control('servico', ['options' => $servicos, 'class' => 'form-select', 'empty' => '-- Selecione --', 'label' => false]) ?>
+            </div>
+            <div class="col-md-2">
+                <label class="form-label">Período:</label>
+                <?= $this->Form->control('periodo', ['type' => 'month', 'class' => 'form-control', 'label' => false]) ?>
+            </div>
+            <div class="col-md-2">
+                <label class="form-label">Funcionário:</label>
+                <?= $this->Form->control('funcionario', ['options' => $funcionarios, 'class' => 'form-select', 'empty' => '--- Selecione ---', 'label' => false]) ?>
+            </div>
+            <div class="col-md-2" style="margin-top: 3.2rem;">
+                <?= $this->Form->button(__('Buscar'), ['class' => 'btn btn-outline-secondary btn-sm btn-shadow']) ?>
+                <?= $this->Html->link(__('Limpar'), ['action' => 'index'], ['class' => 'btn btn-outline-secondary btn-sm btn-shadow']) ?>
+            </div>
         </div>
-        <div class="col-md-2">
-            <label class="form-label">Período:</label>
-            <?= $this->Form->control('periodo', ['type' => 'month', 'class' => 'form-control', 'label' => false]) ?>
-        </div>
-        <div class="col-md-2">
-            <label class="form-label">Funcionário:</label>
-            <?= $this->Form->control('funcionario', ['options' => $funcionarios, 'class' => 'form-select', 'empty' => '--- Selecione ---', 'label' => false]) ?>
-        </div>
-        <div class="col-md-2" style="margin-top: 3.2rem;">
-            <?= $this->Form->button(__('Buscar'), ['class' => 'btn btn-outline-dark btn-sm btn-shadow']) ?>
-            <?= $this->Html->link(__('Limpar'), ['action' => 'index'], ['class' => 'btn btn-outline-dark btn-sm btn-shadow']) ?>
-        </div>
-    </div>
     <?= $this->Form->end() ?>
-    <div class="digitalizacao index content">
-        <?= $this->Html->link(__('Cadastrar'), ['action' => 'add'], ['class' => 'btn btn-secondary float-end mb-4']) ?>
-        <div class="table-responsive table-gpqr">
-            <table class="table table-borderless table-striped text-center align-middle">
-                <thead>
-                    <tr>
-                        <th><?= $this->Paginator->sort('servico_id', ['label' => 'Serviços']) ?></th>
-
-                        <th><?= $this->Paginator->sort('quantidade_documentos') ?></th>
-                        <th><?= $this->Paginator->sort('período do documento') ?></th>
-                        <th> <?= $this->Paginator->sort('funcionario') ?> </th>
-                        <th><?= __('Actions') ?></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($servicosExist as $dtg) : ?>
-                    <tr>
-                        <td><?= $this->Html->link($dtg->servico->nome_servico, ['action' => 'view', $dtg->id], ['class' => 'custom-btn btn-gpqr-view']) ?></td>
-                        <td><?= $this->Number->format($dtg->quantidade_documentos) ?></td>
-                        <td><?= date('m/Y', strtotime($dtg->periodo)) ?></td>
-                        <td><?= h($dtg->funcionario) ?> </td>
-                        <td>
-                            <!-- <?= $this->Html->link(__('<i class="fa-regular fa-pen-to-square fa-lg" style="color: #ffc107"></i>' . __('')), ['action' => 'edit', $dtg->id], ['escape' => false, 'class' => 'p-2']); ?>
-                        <?= $this->Form->postLink(__('<i class="fa-regular fa-trash-can fa-lg" style="color: #dc3545;"></i>' . __('')), ['action' => 'delete', $dtg->id], ['escape' => false, 'class' => 'p-2'], ['confirm' => __('Tem certeza que quer deletar essa Digitalizacao # {0}?', $dtg->id)]) ?>  -->
-                            <?= $this->Html->link(__('Editar'), ['action' => 'edit', $dtg->id], ['class' => 'btn btn-outline-warning btn-sm btn-shadow']) ?>
-                            <?= $this->Form->postLink(__('Excluir'), ['action' => 'delete', $dtg->id], ['class' => 'btn btn-outline-danger btn-sm btn-shadow ', 'confirm' => __('Realmente deseja excluir o serviço:  {0}?', $dtg->id)]) ?>
-
-                        </td>
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-        <?= $this->element('pagination') ?>
+</div>
+<div class="table-gpqr">
+    <table class="table table-borderless table-striped text-center align-middle">
+        <thead class="sticky-top">
+            <tr>
+                <th><?= $this->Paginator->sort('servico_id', ['label' => 'Serviço']) ?></th>
+                <th><?= $this->Paginator->sort('data_digitalizacao', ['label' => 'Cadastro']) ?></th>
+                <th><?= $this->Paginator->sort('funcionario', ['label' => 'Funcionário']) ?></th>
+                <th>Documentos</th>
+                <th><?= $this->Paginator->sort('periodo', ['label' => 'Período']) ?></th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($digitalizacao as $dtg) : ?>
+                <tr>
+                    <td><?= h($dtg->servico->nome_servico) ?></td>
+                    <td><?= h($dtg->data_digitalizacao) ?></td>
+                    <td><?= h($dtg->funcionario) ?></td>
+                    <td><?= $this->Number->format($dtg->quantidade_documentos) ?></td>
+                    <td><?= h($dtg->periodo->format('m/Y')) ?></td>
+                    <td>
+                        <?= $this->Html->link(__('Editar'), ['action' => 'edit', $dtg->id], ['class' => 'btn btn-outline-warning btn-sm btn-shadow']) ?>
+                        <?= $this->Form->postLink(__('Excluir'), ['action' => 'delete', $dtg->id], ['class' => 'btn btn-outline-danger btn-sm btn-shadow ', 'confirm' => __('Realmente deseja excluir o serviço:  {0}?', $dtg->servico->nome_servico)]) ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<?= $this->element('pagination') ?>
