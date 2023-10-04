93da627edece1a9e3bf817ac9bc916826a0a069a
Cristian Braga <crisllopes10@hotmail.com>
2023-10-04 10:03:04 -0300

    feat: Filtros na Passagem de Turno

--- templates/Admin/PassagemTurno/index.php
+++ templates/Admin/PassagemTurno/index.php
@@ -1,6 +1,28 @@
 <h2 class="text-center text-gpqr mt-2 mb-4">PASSAGEM DE TURNO</h2>
-<?= $this->Html->link(__('Cadastrar'), ['action' => 'add'], ['class' => 'btn btn-secondary mb-4']) ?>
-<div class="table-responsive table-gpqr">
+<?= $this->Html->link(__('Cadastrar'), ['action' => 'add'], ['class' => 'btn btn-secondary']) ?>
+<div class="conteudo">
+    <?= $this->Form->create(null, ['type' => 'get']) ?>
+        <div class="row g-3">
+            <div class="col-md-2">
+                <label class="form-label">Etapa:</label>
+                <?= $this->Form->control('etapa', ['options' => $etapas, 'class' => 'form-select', 'empty' => '-- Selecione --', 'label' => false]) ?>
+            </div>
+            <div class="col-md-2">
+                <label class="form-label">Data inicial:</label>
+                <?= $this->Form->control('data_inicio', ['type' => 'date', 'class' => 'form-control', 'label' => false]) ?>
+            </div>
+            <div class="col-md-2">
+                <label class="form-label">Data final:</label>
+                <?= $this->Form->control('data_fim', ['type' => 'date', 'class' => 'form-control', 'label' => false]) ?>
+            </div>
+            <div class="col-md-2" style="margin-top: 3.2rem;">
+                <?= $this->Form->button(__('Buscar'), ['class' => 'btn btn-outline-secondary btn-sm btn-shadow']) ?>
+                <?= $this->Html->link(__('Limpar'), ['action' => 'index'], ['class' => 'btn btn-outline-secondary btn-sm btn-shadow']) ?>
+            </div>
+        </div>
+    <?= $this->Form->end() ?>
+</div>
+<div class="table-gpqr">
     <table class="table table-borderless table-striped text-center align-middle">
         <thead>
             <tr>
